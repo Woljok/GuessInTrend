0190805ab6bb0f71db751ac8087cefb553749493
Furkan <furkandursun947@gmail.com>
2020-12-20 16:20:26 +0300

    some functions added like edit_profile, profile. Register and login functions finished

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from MySQLdb import IntegrityError
 from database import Database
 
+
 app = Flask(__name__)
 app.secret_key = 'woljokerino'
 db = Database("127.0.0.1",3306, "root", "furkan1907", "mydb")
@@ -11,12 +12,106 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    query = "SELECT * FROM mydb.user"
-    db.cursor.execute(query)
-    myresult = db.cursor.fetchall()
-    return render_template("index.html",data=myresult)
+    session["logged"] = False
+    return render_template("index.html")
+
+@app.route("/home")
+def home_page():
+    boolUser = False
+    if session["logged"] == True:
+        boolUser = True
+        return render_template("home.html", boolUser= boolUser, nickname = session["nickname"])
+    else:
+        return render_template("home.html", boolUser=boolUser)
+   
 
 
+@app.route("/profile/<string:userNickname>")
+def profile(userNickname):
+    print("geldi")
+    if session["logged"] == True:
+        query = "SELECT * FROM mydb.user WHERE idUser = \"" + str(session["idUser"]) + "\""
+        db.cursor.execute(query)
+        profileHolder = db.cursor.fetchone()
+        return render_template("profile.html", profile = profileHolder)
+    else:
+        return redirect(url_for("error"))
+
+@app.route("/profile/<string:userNickname>/edit", methods=["GET", "POST"])
+def editProfile(userNickname):
+    if request.method == "GET":
+        if session["logged"] == True:
+            query = "SELECT * FROM mydb.user WHERE idUser = \"" + str(session["idUser"]) + "\""
+            db.cursor.execute(query)
+            profileHolder = db.cursor.fetchone()
+            return render_template("edit_profile.html", profile = profileHolder)
+        else:
+            return redirect(url_for("error"))
+    else:
+        query = "SELECT * FROM mydb.user WHERE idUser = \"" + str(session["idUser"]) + "\""
+        db.cursor.execute(query)
+        profileHolder = db.cursor.fetchone()
+        nickname = request.form.get("nickname")
+        password = request.form.get("password")
+        password2 = request.form.get("passwordNew")
+        password3 = request.form.get("passwordNew2")
+        fullname = request.form.get("fullname")
+        mail = request.form.get("mail")
+        age = request.form.get("age")
+        if(profileHolder[3] != password):
+            return render_template("edit_profile.html", profile= profileHolder, message = 'ŞİFRE YANLIŞ, TEKRAR DENEYİNİZ.')
+        if password2 != '':
+            if password2 != password3:
+                return render_template("edit_profile.html", profile= profileHolder, message = 'ŞİFRELER EŞLEŞMİYOR.')
+        if age != '':
+            if fullname != '':
+                if password2 != '':
+                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
+                    vals = (nickname, fullname, password2, mail, age,str(session["idUser"]))
+                    db.cursor.execute(query, vals)
+                    db.con.commit()
+                else:
+                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s, age = %s WHERE idUser = %s"
+                    vals = (nickname, fullname, mail, age, str(session["idUser"]) )
+                    db.cursor.execute(query, vals)
+                    db.con.commit()
+            else:
+                if password2 != '':
+                    query = "UPDATE mydb.user SET nickname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
+                    vals = (nickname, password2, mail, age,str(session["idUser"]))
+                    db.cursor.execute(query, vals)
+                    db.con.commit()
+                else:
+                    query = "UPDATE mydb.user SET nickname = %s, mail = %s, age = %s WHERE idUser = %s"
+                    vals = (nickname, mail, age,str(session["idUser"]))
+                    db.cursor.execute(query, vals)
+                    db.con.commit()
+        else:
+            if fullname != '':
+                if password2 == '':
+                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s WHERE idUser = %s"
+                    val = (nickname, fullname, mail,str(session["idUser"]))
+                    db.cursor.execute(query, val)
+                    db.con.commit()
+                else:
+                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s WHERE idUser = %s"
+                    val = (nickname, fullname, password2, mail,str(session["idUser"]))
+                    db.cursor.execute(query, val)
+                    db.con.commit()
+            else:
+                if password2 == '':
+                    query = "UPDATE mydb.user SET nickname = %s,mail = %s WHERE idUser = %s"
+                    val = (nickname, mail,str(session["idUser"]))
+                    db.cursor.execute(query, val)
+                    db.con.commit()
+                else:
+                    query = "UPDATE mydb.user SET nickname = %s,password = %s, mail = %s WHERE idUser = %s"
+                    val = (nickname,password2, mail,str(session["idUser"]))
+                    db.cursor.execute(query, val)
+                    db.con.commit()
+        return redirect(url_for("profile", userNickname = nickname))
+
+        
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -36,8 +131,9 @@ def login():
                 if userChecker[3] == password:
                     session["logged"] = True
                     session["idUser"] = userChecker[0]
-                    session["nickname"] = userChecker[1] 
-                    return redirect(url_for("home.html"))
+                    session["nickname"] = userChecker[1]
+                    session.permanent = True 
+                    return redirect(url_for("home_page"))
                 else:    
                     return render_template("login.html",message = 'ŞİFRE YANLIŞ, TEKRAR DENEYİNİZ.')
             else:
@@ -71,30 +167,40 @@ def register():
         if checkUser is not None:
             return render_template("register.html", message="BU KULLANICI ADI KULLANIMDA")
         if age != '':
-            if mail != '':
+            if fullname != '':
                 query = "INSERT INTO mydb.user (nickname, fullname, password, mail, age) VALUES (%s,%s,%s,%s,%s)"
-                val = (nickname, fullname, password, mail, age)
-                db.cursor.execute(query, val)
+                vals = (nickname, fullname, password, mail, age)
+                db.cursor.execute(query, vals)
                 db.con.commit()
             else:
-                query = "INSERT INTO mydb.user (nickname, fullname, password, age) VALUES (%s,%s,%s,%s)"
-                val = (nickname, fullname, password, age)
-                db.cursor.execute(query, val)
+                query = "INSERT INTO mydb.user (nickname, password, mail, age) VALUES (%s,%s,%s,%s)"
+                vals = (nickname, password, mail , age)
+                db.cursor.execute(query, vals)
                 db.con.commit()
         else:
-            if mail != '':
+            if fullname != '':
                 query = "INSERT INTO mydb.user (nickname, fullname, password, mail) VALUES (%s,%s,%s,%s)"
-                val = (nickname, fullname, password, mail)
-                db.cursor.execute(query, val)
+                vals = (nickname, fullname, password, mail)
+                db.cursor.execute(query, vals)
                 db.con.commit()
             else:
-                query = "INSERT INTO mydb.user (nickname, fullname, password) VALUES (%s,%s,%s)"
-                vals = (nickname, fullname, password)
+                query = "INSERT INTO mydb.user (nickname, password, mail) VALUES (%s,%s,%s)"
+                vals = (nickname, password, mail)
                 db.cursor.execute(query, vals)
                 db.con.commit()
         return render_template("register.html", message = "BAŞARIYLA KAYIT OLDUNUZ")
 
 
+@app.route("/logout")
+def logout():
+    session["logged"] = False
+    session["nickname"] = False
+    session["IdUser"] = False
+    return redirect(url_for("home"))
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
 
 
 if __name__ == '__main__':
