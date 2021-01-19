58f4602c298d6be25af16d877aa248f7b02c8371
FED <frknwoljok@gmail.com>
2021-01-19 20:18:57 +0300

    some functions added

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import os
 from werkzeug.utils import secure_filename
 path = os.getcwd()
 UPLOAD_FOLDER = os.path.join(path, 'static\\img')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.secret_key = 'woljokerino'
 db = Database("127.0.0.1",3306, "root", "furkan1907", "mydb")
@@ -24,7 +25,10 @@ def index():
 @app.route("/home")
 def home_page():
     if session["logged"] == True:
-        return render_template("home.html")
+        query = "SELECT * FROM mydb.user WHERE idUser = " + str(session["idUser"])
+        db.cursor.execute(query)
+        userFound = db.cursor.fetchone()
+        return render_template("home.html", user = userFound)
     else:
         return render_template("index.html")
    
@@ -67,60 +71,79 @@ def editProfile(userNickname):
         if password2 != '':
             if password2 != password3:
                 return render_template("edit_profile.html", profile= profileHolder, message = 'ŞİFRELER EŞLEŞMİYOR.')
-               
-        if age != '':
-            if fullname != '':
-                if password2 != '':
-                    hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
-                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
-                    vals = (nickname, fullname, hashedPass, mail, age,str(session["idUser"]))
-                    db.cursor.execute(query, vals)
-                    db.con.commit()
+        if request.form.get("change") == "changeProfile":
+            if age != '':
+                if fullname != '':
+                    if password2 != '':
+                        hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
+                        query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
+                        vals = (nickname, fullname, hashedPass, mail, age,str(session["idUser"]))
+                        db.cursor.execute(query, vals)
+                        db.con.commit()
+                    else:
+                        query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s, age = %s WHERE idUser = %s"
+                        vals = (nickname, fullname, mail, age, str(session["idUser"]) )
+                        db.cursor.execute(query, vals)
+                        db.con.commit()
                 else:
-                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s, age = %s WHERE idUser = %s"
-                    vals = (nickname, fullname, mail, age, str(session["idUser"]) )
-                    db.cursor.execute(query, vals)
-                    db.con.commit()
+                    if password2 != '':
+                        hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
+                        query = "UPDATE mydb.user SET nickname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
+                        vals = (nickname, hashedPass, mail, age,str(session["idUser"]))
+                        db.cursor.execute(query, vals)
+                        db.con.commit()
+                    else:
+                        query = "UPDATE mydb.user SET nickname = %s, mail = %s, age = %s WHERE idUser = %s"
+                        vals = (nickname, mail, age,str(session["idUser"]))
+                        db.cursor.execute(query, vals)
+                        db.con.commit()
             else:
-                if password2 != '':
-                    hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
-                    query = "UPDATE mydb.user SET nickname = %s, password = %s, mail = %s, age = %s WHERE idUser = %s"
-                    vals = (nickname, hashedPass, mail, age,str(session["idUser"]))
-                    db.cursor.execute(query, vals)
-                    db.con.commit()
+                if fullname != '':
+                    if password2 == '':
+                        
+                        query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s WHERE idUser = %s"
+                        val = (nickname, fullname, mail,str(session["idUser"]))
+                        db.cursor.execute(query, val)
+                        db.con.commit()
+                    else:
+                        hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
+                        query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s WHERE idUser = %s"
+                        val = (nickname, fullname, hashedPass, mail,str(session["idUser"]))
+                        db.cursor.execute(query, val)
+                        db.con.commit()
                 else:
-                    query = "UPDATE mydb.user SET nickname = %s, mail = %s, age = %s WHERE idUser = %s"
-                    vals = (nickname, mail, age,str(session["idUser"]))
-                    db.cursor.execute(query, vals)
-                    db.con.commit()
+                    if password2 == '':
+                        
+                        query = "UPDATE mydb.user SET nickname = %s,mail = %s WHERE idUser = %s"
+                        val = (nickname, mail,str(session["idUser"]))
+                        db.cursor.execute(query, val)
+                        db.con.commit()
+                    else:
+                        hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
+                        query = "UPDATE mydb.user SET nickname = %s,password = %s, mail = %s WHERE idUser = %s"
+                        val = (nickname,hashedPass, mail,str(session["idUser"]))
+                        db.cursor.execute(query, val)
+                        db.con.commit()
+            return redirect(url_for("profile", userNickname = nickname))
         else:
-            if fullname != '':
-                if password2 == '':
-                    
-                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, mail = %s WHERE idUser = %s"
-                    val = (nickname, fullname, mail,str(session["idUser"]))
-                    db.cursor.execute(query, val)
-                    db.con.commit()
-                else:
-                    hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
-                    query = "UPDATE mydb.user SET nickname = %s, fullname = %s, password = %s, mail = %s WHERE idUser = %s"
-                    val = (nickname, fullname, hashedPass, mail,str(session["idUser"]))
-                    db.cursor.execute(query, val)
-                    db.con.commit()
-            else:
-                if password2 == '':
-                    
-                    query = "UPDATE mydb.user SET nickname = %s,mail = %s WHERE idUser = %s"
-                    val = (nickname, mail,str(session["idUser"]))
-                    db.cursor.execute(query, val)
-                    db.con.commit()
-                else:
-                    hashedPass = bcrypt.hashpw(password2,bcrypt.gensalt()) 
-                    query = "UPDATE mydb.user SET nickname = %s,password = %s, mail = %s WHERE idUser = %s"
-                    val = (nickname,hashedPass, mail,str(session["idUser"]))
-                    db.cursor.execute(query, val)
-                    db.con.commit()
-        return redirect(url_for("profile", userNickname = nickname))
+            query = "DELETE FROM mydb.rank WHERE userId = " + str(session["idUser"])
+            db.cursor.execute(query)
+            db.con.commit()
+            db.cursor = db.con.cursor(buffered=True)
+            query = "DELETE FROM mydb.user_play_bet WHERE user_id = " + str(session["idUser"])
+            db.cursor.execute(query)
+            db.con.commit()
+            db.cursor = db.con.cursor(buffered=True)
+            query = "DELETE FROM mydb.comment WHERE user_id = " + str(session["idUser"])
+            db.cursor.execute(query)
+            db.con.commit()
+            db.cursor = db.con.cursor(buffered=True)
+            query = "DELETE FROM mydb.user WHERE idUser = " + str(session["idUser"])
+            db.cursor.execute(query)
+            db.con.commit()
+            return render_template("index.html")
+
+        
 
 
 @app.route("/bets")
@@ -128,7 +151,100 @@ def bets():
     query = "SELECT * FROM mydb.bet ORDER BY time"
     db.cursor.execute(query)
     bets = db.cursor.fetchall()
-    return render_template("bets.html", bets = bets)
+    query = "SELECT * FROM mydb.category ORDER BY nameCategory"
+    db.cursor.execute(query)
+    categories = db.cursor.fetchall()
+    lenBet = len(bets)
+    lenCat = len(categories)
+    currentDate = datetime.today()
+    return render_template("bets.html", bets = bets, date= currentDate , cats = categories, lenBet = lenBet, lenCat = lenCat)
+
+
+@app.route("/bets/<categoryName>")
+def bets_category(categoryName):
+    query = "SELECT * FROM mydb.category WHERE nameCategory = \"" + str(categoryName) + "\""
+    db.cursor.execute(query)
+    cat = db.cursor.fetchone()
+    catId = cat[0]
+    query = "SELECT * FROM mydb.bet_has_category WHERE category_id = " + str(catId)
+    db.cursor.execute(query)
+    betsIds = db.cursor.fetchall()
+
+    betArray = []
+    for i in range(len(betsIds)):
+        query = "SELECT * FROM mydb.bet WHERE idBets = \"" + str(betsIds[i][0]) + "\" ORDER BY mydb.bet.time"
+        db.cursor.execute(query)
+        betx = db.cursor.fetchone()
+        print(betx)
+        betArray.append(betx)
+    
+    query = "SELECT * FROM mydb.category ORDER BY nameCategory"
+    db.cursor.execute(query)
+    categories = db.cursor.fetchall()
+    lenBet = len(betArray)
+    lenCat = len(categories)
+    currentDate = datetime.today()
+    return render_template("bets.html", bets=betArray, date= currentDate ,cats = categories, lenBet = lenBet, lenCat = lenCat)
+
+@app.route("/bets/bet/<betId>", methods=["GET","POST"])
+def bets_show(betId):
+    if session["logged"] == False and session["admin"] == False:
+        return render_template("login.html")
+    query = "SELECT * FROM mydb.bet WHERE idBets = \"" + str(betId) + "\""
+    db.cursor.execute(query)
+    bet = db.cursor.fetchone()
+    query = "SELECT * FROM mydb.category ORDER BY nameCategory"
+    db.cursor.execute(query)
+    categories = db.cursor.fetchall()
+    db.cursor = db.con.cursor(buffered=True)
+    lenCat = len(categories)
+    query = "SELECT * FROM mydb.user_play_bet WHERE user_id = " + str(session["idUser"]) + " AND bet_id = " + str(betId)
+    db.cursor.execute(query)
+    isPlayed = db.cursor.fetchone() 
+    db.cursor = db.con.cursor(buffered=True)
+    query = "SELECT comment.idComment, comment.user_id, comment.bet_id, comment.Text, comment.time, user.nickname FROM mydb.comment LEFT JOIN mydb.user ON comment.user_id = user.idUser WHERE bet_id = \"" + str(betId) + "\""
+    db.cursor.execute(query)
+    comments = db.cursor.fetchall()
+    lenCom = len(comments)
+    db.cursor = db.con.cursor(buffered=True)
+    comDelete = request.form.getlist("delete")
+    commentarea = request.form.get("commentArea")
+    print(commentarea)
+    print(comDelete)
+    if request.method == "GET":   
+        print("asdsad")   
+        return render_template("betAnswer.html", bet = bet,cats = categories, lenCat = lenCat, isPlayed = isPlayed, comments = comments, lenCom = lenCom)
+    else:
+        if request.form.get("showBet") == "answerBet":
+            answer = request.form.get("answer")
+            if answer == "yes":
+                answer = 0
+            else:
+                answer = 1
+            query = "INSERT INTO mydb.user_play_bet (user_id, bet_id, answer) VALUES (" + str(session["idUser"]) + ", "+ str(betId) +", "+ str(answer) +" )"    
+            db.cursor.execute(query)
+            db.con.commit()
+            flash("ANSWERED SUCCESSFULLY","success")
+            return redirect(url_for("bets_show", betId = betId))
+        elif request.form.get("showBet") == "addComment":
+            print("geldiasdasd")
+            
+            
+            query = "INSERT INTO mydb.comment (user_id, bet_id, Text,time) VALUES (%s, %s, %s, current_timestamp())"
+            vals = (session["idUser"], betId, commentarea)   
+            db.cursor.execute(query, vals)
+            db.con.commit()   
+            return redirect(url_for("bets_show", betId = betId))
+        else:
+            print(comDelete)
+            print("asdsa")
+            for f in comDelete:
+                query = "DELETE FROM mydb.comment WHERE idComment  = " + str(f)
+                print(query)
+                db.cursor.execute(query)
+                db.con.commit()
+            return redirect(url_for("bets_show", betId = betId))
+            
 
 
 
@@ -206,7 +322,6 @@ def edit_bets():
         db.cursor.execute(query)
         bets = db.cursor.fetchall()
         lenBets = len(bets)
-        print(lenBets)
         currentDate = datetime.today()
         return render_template("edit_bets.html", bets=bets, lenBets = lenBets, date = currentDate)
 
@@ -224,7 +339,7 @@ def edit_bet(betId):
         db.cursor.execute(query)
         bet = db.cursor.fetchone()
         question = request.form.get("question")
-        reward = request.form.get("reward")
+        reward = int(request.form.get("reward"))
         file = request.files['file']
         answer = request.form.get("radio")
         if answer == "dont":
@@ -235,23 +350,29 @@ def edit_bet(betId):
                 val = (question,filename1,reward,bet[0])
                 db.cursor.execute(query, val)
                 db.con.commit()
-                print("eldi")
-                print(file)
             else:
                 query = "UPDATE mydb.bet SET question = %s, reward = %s WHERE idBets = %s"
                 val = (question,reward,bet[0])
                 db.cursor.execute(query, val)
                 db.con.commit()
         else:
-            query = "SELECT user_id FROM mydb.user_play_bet WHERE bet_id = \"" + bet[0] + "\""
+            query = "SELECT * FROM mydb.user_play_bet WHERE bet_id = \"" + str(bet[0]) + "\""
             db.cursor.execute(query)
-            users = db.cursor.fetchone()
+            users = db.cursor.fetchall()
+            db.cursor = db.con.cursor(buffered=True)
+            query = "UPDATE mydb.bet SET answer = %s WHERE idBets = %s"
+            vals = (answer, bet[0])
+            db.cursor.execute(query,vals)
+            db.con.commit()
+
             if users:
                 for i in range(0, len(users)):
-                    if users[i][2] == answer:
-                        query = "SELECT * FROM mydb.user WHERE idUser = \"" + users[i] +"\""
+                    a = users[i][2]
+                    if answer == str(a):
+                        query = "SELECT * FROM mydb.user WHERE idUser = \"" + str(users[i][0]) +"\""
                         db.cursor.execute(query)
                         user = db.cursor.fetchone()
+                        user = list(user)
                         user[6] = user[6] + reward
                         query = "UPDATE mydb.user SET coin = %s WHERE idUser = %s"
                         val = (user[6], user[0])
@@ -259,7 +380,35 @@ def edit_bet(betId):
                         db.con.commit() 
         return render_template("edit_panel.html")
 
-
+@app.route("/editPanel/delete", methods=["GET","POST"])
+def delete_bet():
+    if session["admin"] == False:
+        return render_template("error.html")
+    if request.method == "GET":
+        query = "SELECT * FROM mydb.bet ORDER BY bet.time"
+        db.cursor.execute(query)
+        bets = db.cursor.fetchall()
+        lenBets = len(bets)
+        currentDate = datetime.today()
+        return render_template("delete_bet.html",bets = bets,lenBets = lenBets, date=currentDate)
+    else:
+        deleteBets = request.form.getlist("betDelete")
+        if deleteBets:
+            for i in range(0, len(deleteBets)):
+                query = "DELETE FROM mydb.bet_has_category WHERE bet_id = " + str(deleteBets[i])
+                db.cursor.execute(query)
+                db.con.commit()
+                query = "DELETE FROM mydb.comment WHERE bet_id = " + str(deleteBets[i])
+                db.cursor.execute(query)
+                db.con.commit()
+                query = "DELETE FROM mydb.user_play_bet WHERE bet_id = " + str(deleteBets[i])
+                db.cursor.execute(query)
+                db.con.commit()
+                query = "DELETE FROM mydb.bet WHERE idBets = " + str(deleteBets[i])
+                db.cursor.execute(query)
+                db.con.commit()
+                
+        return render_template("edit_panel.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -284,11 +433,11 @@ def login():
                         session["admin"] = True 
                     return redirect(url_for("home_page"))
                 else:    
-                    return render_template("login.html",message = 1)
+                    return render_template("login.html",message = "Your password is wrong!")
             else:
-                return render_template("login.html", message = 2)
+                return render_template("login.html", message = "There is no user with this nickname!")
         else:
-            return render_template("login.html", message = 3)
+            return render_template("login.html", message = "Fill the form!")
 
 
 @app.route("/register", methods=["POST","GET"])
@@ -346,6 +495,26 @@ def register():
         db.cursor.execute(query)
         db.con.commit()
         return render_template("register.html", message = "BAŞARIYLA KAYIT OLDUNUZ")
+
+
+@app.route("/faq", methods=["GET", "POST"])
+def faq():
+    if request.method == "GET":
+        return render_template("faq.html")
+    else:
+        if session["logged"] == False:
+            return render_template("login.html")
+        else:
+            text = request.form.get("text")
+            query = "INSERT INTO mydb.ticket (user_id, text, time) VALUES (%s, %s, current_timestamp())"
+            vals = (session["idUser"], text)
+            db.cursor.execute(query, vals)
+            db.con.commit()
+            msg = "Your ticket has been sent. We will send our respond to your mail. Don't forget to check your mail. See you!"
+            return render_template("faq.html", message = msg)
+
+
+
 
 
 @app.route("/logout")
